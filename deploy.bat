2b58c3fcd8922e082a64e9be37377e02d07d2dda
Robiul Awal <robiulawal154@gmail.com>
2026-08-26 02:09:41 +0600

    Protect live production database by removing seed execution from build script

--- deploy.bat
+++ deploy.bat
@@ -5,7 +5,6 @@ echo ==========================================
 
 call npm install --production
 call npm run migrate
-call npm run seed
 
 pm2 restart mfi-management || pm2 start app.js --name "mfi-management"
 pm2 save
